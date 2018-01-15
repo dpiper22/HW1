e5c1cb2dd57cd20835974983b532516770ed95c6
dpiper22 <32075261+dpiper22@users.noreply.github.com>
2018-01-14 21:27:39 -0500

    Add files via upload

--- SI364W18_HW1.py
+++ SI364W18_HW1.py
@@ -6,18 +6,105 @@
 
 ## List below here, in a comment/comments, the people you worked with on this assignment AND any resources you used to find code (50 point deduction for not doing so). If none, write "None".
 
-
+#David Piper
+#Worked with: Julia Pezzullo
+#resources: iTunes Search API: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 
 ## [PROBLEM 1] - 150 points
 ## Below is code for one of the simplest possible Flask applications. Edit the code so that once you run this application locally and go to the URL 'http://localhost:5000/class', you see a page that says "Welcome to SI 364!"
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 app.debug = True
-
+import requests
+import json
+app = Flask(__name__)
+app.debug = True
+import base64
 @app.route('/')
 def hello_to_you():
     return 'Hello!'
+#problem 1
+
+
+@app.route('/class')
+def hello_class():
+	return "Welcome to SI 364!"
+
+
+#problem 2
+
+@app.route('/movie/<anytitlesearch>')
+def movie_info(anytitlesearch):
+	finds = requests.get('http://itunes.apple.com/search?', params = {'term': anytitlesearch})
+	return finds.text
+
+
+
+#problem 3
+
+@app.route('/question')
+def question():
+		q = """<!DOCTYPE html>
+	<html>
+	<body>
+	<form action="/result" method="post">
+	Enter your favorite number: <br>
+	<input type="int" name="favorite_number">
+	<br>
+	<input type="submit" value="Submit">
+	</form>
+	</body>
+	</html>"""
+		return q
+@app.route('/result', methods=['POST', 'GET'])
+def favnumb():
+	if request.method == 'POST':
+		numb = request.form["favorite_number"]
+		double_numb = int(numb) * 2
+		return "Double your favorite number is " + str(double_numb)
+
+
+
+#problem 4
+
+
+@app.route('/problem4form', methods=['GET', 'POST'])
+def prob4():
+	bk = """<!DOCTYPE html>
+		<html>
+		<body>
+		<form action="/problem4form" method="POST">
+		<h1>Book Search</h1>
+		What book genre do you prefer?<br>
+		<input type="checkbox" name="Genre" value="Fiction">Fiction<br>
+		<input type="checkbox" name="Genre" value="Non-Fiction">Non-Fiction<br>
+		<input type="checkbox" name="Genre" value="Fantasy">Fantasy<br>
+		<input type="checkbox" name="Genre" value="Adventure">Adventure<br>
+		Who is your favorite author? <br><input type="text" name="author"<br>
+		<input type="submit" value="Submit">
+		</form>
+		</body>
+		</html>"""
+	if request.method=='POST':
+		authors_b = []
+		feedback = str(request.form['author'])
+		book_data = requests.get('http://itunes.apple.com/search?', params = {'term': feedback, 'entity': 'audiobook',})
+		json_stuff = json.loads(book_data.text)
+		for b in json_stuff['results']:
+			books = b['collectionName']
+			authors_b.append(books)
+		return "Your favorite author is {}. Some books they have written are {}".format(feedback, str(authors_b)) + bk
+	else:
+		return bk
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
